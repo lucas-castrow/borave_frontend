236a525296d8053ae5751cfdfb1e6e22221f0235
lucas-castrow <33181463+lucas-castrow@users.noreply.github.com>
2023-10-31 18:56:19 -0300

    removing creds aws to avoid github messages

--- src/utils/aws-exports.ts
+++ src/utils/aws-exports.ts
@@ -1,11 +1,11 @@
-import {S3} from 'aws-sdk';
+// import {S3} from 'aws-sdk';
 
-const aws_s3 = {
-  accessKeyId: 'AKIA2UVW3BCLXRLWRTUW',
-  secretAccessKey: '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',
-  region: 'sa-east-1',
-};
+// const aws_s3 = {
+//   accessKeyId: '',
+//   secretAccessKey: '',
+//   region: '',
+// };
 
-export const bucketName = 'borave-posts';
+// export const bucketName = 'borave-posts';
 
-export const s3 = new S3(aws_s3);
+// export const s3 = new S3(aws_s3);
